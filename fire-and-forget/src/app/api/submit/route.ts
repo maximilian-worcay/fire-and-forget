53eae20c93e0177243c656244e87e0f7177f13af
maximilian-worcay <150015854+maximilian-worcay@users.noreply.github.com>
2024-05-15 12:01:20 +0200

    Logging Response Code now

--- fire-and-forget/src/app/api/submit/route.ts
+++ fire-and-forget/src/app/api/submit/route.ts
@@ -4,13 +4,14 @@ export async function POST(request: NextRequest) {
     const data = await request.json()
     const url = `${getUrl()}/api/job`
     console.log("Sending data to job route: ", url)
-    await fetch(url, {
+    const response = await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
     })
+    console.log("Response status from job route: ", response.status)
     return NextResponse.json(data)
 }
 
