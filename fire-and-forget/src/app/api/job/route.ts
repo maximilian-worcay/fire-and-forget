a2805d6d440b3c0af676e8f16a9a11c39aa52e0f
maximilian-worcay <150015854+maximilian-worcay@users.noreply.github.com>
2024-05-15 12:15:00 +0200

    Removed Awaiting the fetch and waiting 5 seconds now

--- fire-and-forget/src/app/api/job/route.ts
+++ fire-and-forget/src/app/api/job/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function POST(request: NextRequest) {
     const data = await request.json()
     console.log("Entered job route")
-    await new Promise(resolve => setTimeout(resolve, 3000))
-    console.log("3 seconds passed");
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    console.log("5 seconds passed");
     return NextResponse.json(data)
 }
--- fire-and-forget/src/app/api/submit/route.ts
+++ fire-and-forget/src/app/api/submit/route.ts
@@ -4,14 +4,13 @@ export async function POST(request: NextRequest) {
     const data = await request.json()
     const url = `${getUrl()}/api/job`
     console.log("Sending data to job route: ", url)
-    const response = await fetch(url, {
+    fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
     })
-    console.log("Response status from job route: ", response.status)
     return NextResponse.json(data)
 }
 
